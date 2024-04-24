93016c4f05f15daa6d1f164064694aa879e83c41
Bushra Patel <bpatel60@yahoo.com>
2024-04-23 22:15:41 -0400

    Call the nextISSTimeForMyLocation function by passing it a call back to receive the error or pass times accordingly. Use a helper function to print the date/time and duration of the pass times in a proper format

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -26,3 +26,32 @@ const { fetchISSFlyOverTimes } = require('./iss');
 
 //   console.log("It worked! Returned flyover times:", passInfo);
 // });
+
+/**
+ * Input:
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns:
+ *   undefined
+ * Sideffect:
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
